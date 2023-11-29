1718261f8cc1d9e8c97f3d77dc0af11a76683faf
Vinitpal <vs361017@gmail.com>
2023-11-29 23:53:38 +0530

    updated api path

--- frontend/db/path.js
+++ frontend/db/path.js
@@ -1,3 +1,2 @@
-export const expired_old_api_path = "https://carsNcars.online/moto-admin";
-export const expired_api_path = "https://admin.carsNcars.in";
-export const api_path = "http://localhost:5000";
+// export const api_path = "http://localhost:5000";
+export const api_path = "https://carsncars-backend.onrender.com";
